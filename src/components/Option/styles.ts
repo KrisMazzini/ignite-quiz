4a1e84e3b3aeebf8410c743ec225e450a3d00753
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2024-01-31 08:31:15 -0300

    feat(question-option): add an animated Check element to show when an option is selected

--- src/components/Option/styles.ts
+++ src/components/Option/styles.ts
@@ -1,5 +1,5 @@
-import { StyleSheet } from 'react-native';
-import { THEME } from '../../styles/theme';
+import { StyleSheet } from 'react-native'
+import { THEME } from '../../styles/theme'
 
 export const styles = StyleSheet.create({
   container: {
@@ -13,6 +13,7 @@ export const styles = StyleSheet.create({
     padding: 16,
     marginBottom: 12,
   },
+
   title: {
     color: THEME.COLORS.GREY_100,
     fontFamily: 'Roboto_400Regular',
@@ -20,8 +21,9 @@ export const styles = StyleSheet.create({
     marginRight: 32,
     flex: 1,
   },
+
   checked: {
     borderWidth: 1,
-    borderColor: THEME.COLORS.BRAND_LIGHT
-  }
-});
+    borderColor: THEME.COLORS.BRAND_LIGHT,
+  },
+})
